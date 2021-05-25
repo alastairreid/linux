8bb636f999acf2b56d6ffe2893af3a5daa9ac233
Alastair Reid <adreid@google.com>
2021-05-25 15:44:40 +0000

    stubs for rust_helpers

--- rust/stubs.c
+++ rust/stubs.c
@@ -1,4 +1,5 @@
 #include <linux/kernel.h>
+#include <linux/sched/signal.h>
 // #include <linux/compiler_types.h>
 
 // note that the following cannot be written in C because of the
@@ -9,5 +10,28 @@ int printk(const char *fmt, ...) {
     // which is not as useful as I had hoped.
     extern void klee_print_expr(const char *msg, int _dummy);
     klee_print_expr(fmt, 0);
+    return 0;
+}
+
+unsigned long rust_helper_copy_from_user(void *to, const void __user *from, unsigned long n)
+{
+	return 0;
+}
+
+unsigned long rust_helper_copy_to_user(void __user *to, const void *from, unsigned long n)
+{
+	return 0;
+}
+
+void rust_helper_init_wait(struct wait_queue_entry *wq_entry)
+{
+}
+
+void rust_helper_kunmap(struct page *page)
+{
+}
+
+int rust_helper_signal_pending(void)
+{
     return 0;
 }
